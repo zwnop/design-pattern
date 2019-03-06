4b7b7a6f784bb291aff10851f4e87615b9913d06
zwnop <1043786903@qq.com>
2019-03-06 20:35:08 +0800

    Initial commit

---
+++ src/main/java/com/zwnop/App.java
@@ -0,0 +1,13 @@
+package com.zwnop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
